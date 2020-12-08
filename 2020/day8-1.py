4486b1385ddd548732fcacde08bfda0754e6185b
Jocevičius, Karolis <karolis.jocevicius@seb.se>
2020-12-08 16:09:56 +0200

    2020 day 8

---
+++ 2020/day8-1.py
@@ -0,0 +1,30 @@
+
+
+def executeProgramUntilRepeat(program):
+    programPointer = 0
+    acc = 0
+
+    programPointerHistory = set([])
+
+    while (programPointer not in programPointerHistory):
+        programPointerHistory.add(programPointer)
+        command = program[programPointer].split(" ")
+        
+        if command[0] == "nop":
+            programPointer += 1
+        elif command[0] == "jmp":
+            delta = int(command[1])
+            programPointer += delta
+        elif command[0] == "acc":
+            delta = int(command[1])
+            acc += delta
+            programPointer += 1
+
+    return acc
+
+myBagColor = "shiny gold"
+input_file = open("2020/day8.input", "r")
+content_list = input_file.readlines()
+result = executeProgramUntilRepeat(content_list)
+
+print(result)
---
+++ 2020/day8-2.py
@@ -0,0 +1,82 @@
+
+
+def executeProgramUntilRepeat(program):
+    programLen = len(program)
+    programPointer = 0
+    acc = 0
+
+    programPointerHistory = []
+
+    while (programPointer >= 0 and programPointer < programLen):
+        if (programPointer in programPointerHistory):
+            previousPointer = programPointerHistory[len(programPointerHistory)-1]
+            # print("Loop happened on PP: ", programPointer)
+            # print("programPointerHistory: ", programPointerHistory)
+            # print("Previous pointer: ", previousPointer)
+            return programPointerHistory
+
+        programPointerHistory.append(programPointer)
+        command = program[programPointer]
+        command = command.split(" ")
+        
+        if command[0] == "nop":
+            programPointer += 1
+        elif command[0] == "jmp":
+            delta = int(command[1])
+            programPointer += delta
+        elif command[0] == "acc":
+            delta = int(command[1])
+            acc += delta
+            programPointer += 1
+        else:
+            raise Exception("Unknown command " + command)
+
+    print("Program completed!", acc)
+    return acc
+
+# def fixProgram(program, errorIndex):
+#     command = program[errorIndex]
+#     print("Fixing command: ", command)
+#     if (command.startswith("jmp")):
+#         command = command.replace("jmp", "nop")
+#     elif command.startswith("nop"):
+#         command = command.replace("nop", "jmp")
+#     else:
+#         raise Exception("Unknown command: " + command)
+
+#     program = program.copy()
+#     program[errorIndex] = command
+#     return program
+
+
+def fixProgram(program, executionHistory):
+
+    for commandIndex in executionHistory:
+        command = program[commandIndex].strip()
+        # print("Trying to fix command: ", commandIndex, command)
+        if (command.startswith("jmp")):
+            command = command.replace("jmp", "nop")
+        elif command.startswith("nop"):
+            command = command.replace("nop", "jmp")
+        else:
+            continue
+
+        fixedProgram = program.copy()
+        fixedProgram[commandIndex] = command
+
+        fixedResult = executeProgramUntilRepeat(fixedProgram)
+
+        if (fixedResult is not None and isinstance(fixedResult, int)):
+            return fixedResult
+
+    raise Exception("No success fixing program :(")
+
+
+myBagColor = "shiny gold"
+input_file = open("2020/day8.input", "r")
+content_list = input_file.readlines()
+
+executionHistory = executeProgramUntilRepeat(content_list)
+result = fixProgram(content_list, executionHistory)
+
+print(result)
