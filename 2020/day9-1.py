6578481cc5aa04c012d9c484b4963323b6ff75f7
Jocevičius, Karolis <karolis.jocevicius@seb.se>
2020-12-09 14:02:50 +0200

    2020 day 9

---
+++ 2020/day9-1.py
@@ -0,0 +1,40 @@
+
+def loadNumbers(content_lines):
+    numbers_list = []
+
+    for line in content_list:
+        numbers_list.append(int(line))
+
+    print("loaded numbers: ", len(numbers_list))
+    return numbers_list
+
+def isValidNumber(number, numbers_list, pre_range):
+    for i in pre_range:
+        for j in pre_range:
+            if i == j:
+                continue
+            
+            sum = numbers_list[i] + numbers_list[j]
+
+            if number == sum:
+                return True
+    return False
+
+def validateNumbers(numbers_list, preamble_len):
+    for i in range(preamble_len, len(numbers_list)):
+        number_to_validate = numbers_list[i]
+        pre_range = range(i - preamble_len, i)
+        result = isValidNumber(number_to_validate, numbers_list, pre_range)
+
+        if not result:
+            return numbers_list[i]
+
+
+PREAMBLE_LEN = 25
+input_file = open("2020/day9.input", "r")
+content_list = input_file.readlines()
+numbers_list = loadNumbers(content_list)
+
+invalidNumber = validateNumbers(numbers_list, PREAMBLE_LEN)
+
+print(invalidNumber)
---
+++ 2020/day9-2.py
@@ -0,0 +1,43 @@
+
+def loadNumbers(content_lines):
+    numbers_list = []
+
+    for line in content_list:
+        numbers_list.append(int(line))
+
+    print("loaded numbers: ", len(numbers_list))
+    return numbers_list
+
+
+def findContiguousSetForNumber(numbers_list, invalid_number):
+    for i in range(0, len(numbers_list)):
+        sumOfSet = 0
+        setOfNumbers = set([])
+        for j in range(i, len(numbers_list)):
+            number = numbers_list[j]
+
+            if (number == invalid_number):
+                raise Exception("Reached invalid_number :(")
+
+            sumOfSet += number
+
+            if sumOfSet > invalid_number:
+                break
+
+            setOfNumbers.add(number)
+
+            if sumOfSet == invalid_number:
+                return setOfNumbers
+            
+            
+
+
+INVALID_NUMBER = 731031916
+input_file = open("2020/day9.input", "r")
+content_list = input_file.readlines()
+numbers_list = loadNumbers(content_list)
+
+setOfNumbers = findContiguousSetForNumber(numbers_list, INVALID_NUMBER)
+result = min(setOfNumbers) + max(setOfNumbers)
+
+print(result)
