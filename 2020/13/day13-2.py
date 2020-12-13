6e45c06b8fa29391e6223323f54b14c30d0f4d98
Karolis Jocevičius <karolis.jocevicius@gmail.com>
2020-12-13 20:21:20 +0200

    12-2 partial

--- 2020/13/day13-2.py
+++ 2020/13/day13-2.py
@@ -1,15 +1,34 @@
 
-def calc_next_bus_time(timestamp: int, bus_id: int):
-    previous_bus_time = int(timestamp / bus_id) * bus_id
-    next_bus_time = previous_bus_time + bus_id
-    return next_bus_time
+def solve_silly(bus_lines: list):
+
+    result = None
+    base_line_id = bus_lines[0]
+    time = 0
+
+    while result == None:
+        time += base_line_id
+        successes = 0
+
+        for i in range(1, len(bus_lines)):
+            bus_line = bus_lines[i]
+            if (bus_line is not int):
+                continue
+
+            subtime = time + i
+            mod = subtime % bus_line
+            if mod != 0:
+                break
+            else:
+                successes += 1
+
+        if successes == len(bus_lines) - 2:
+            result = time
+
+    return result
 
 
-input_file = open("2020/13/day13.input", "r")
+input_file = open("2020/13/day13-dummy.input", "r")
 content_list = input_file.readlines()
 bus_lines = list(
-    map(int, filter(lambda v: v.isdigit(), content_list[1].split(','))))
-next_bus_times = list(
-    map(lambda v: calc_next_bus_time(timestamp, v), bus_lines))
-print(timestamp, bus_lines)
-print(next_bus_times)
+    map(lambda v: int(v) if v.isdigit() else v, content_list[1].split(',')))
+solve_silly(bus_lines)
